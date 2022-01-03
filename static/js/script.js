e1cbe63f93f81e7fa22572172ecddeaf86d01c06
DatoneX <cgomwe@ymail.com>
2022-01-03 21:03:46 +0000

    Refactor navigation bar styling

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,21 @@
-// Navigation
+// // Navigation
 
-(function ($) {
-  $('.search-btn').on('click', function (e) {
-    if ($('.search-input-container').hasClass('hide')) {
-      e.preventDefault();
-      $('.search-input-container').removeClass('hide');
+// (function ($) {
+//   $('.search-btn').on('click', function (e) {
+//     if ($('.search-input-container').hasClass('hide')) {
+//       e.preventDefault();
+//       $('.search-input-container').removeClass('hide');
 
-      return false;
-    }
-  });
+//       return false;
+//     }
+//   });
 
-  $('.hide-search-input-container').on('click', function (e) {
-    e.preventDefault();
-    $('.search-input-container').addClass('hide');
+//   $('.hide-search-input-container').on('click', function (e) {
+//     e.preventDefault();
+//     $('.search-input-container').addClass('hide');
 
-    return false;
-  });
-})(jQuery);
+//     return false;
+//   });
+// })(jQuery);
 
-// add navbar-lg to hide.  show
+// // add navbar-lg to hide.  show
