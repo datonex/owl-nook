3e46c4f854eb1deee5b9a82f4beaf9d0b9a3c7cd
DatoneX <cgomwe@ymail.com>
2022-01-09 22:29:24 +0000

    change function view to class view

--- owlnookuser/urls.py
+++ owlnookuser/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
-from .views import registration_view
+from .views import UserRegisterView
 
 urlpatterns = [
-    path("register/", registration_view, name="register"),
+    path("register/", UserRegisterView.as_view(), name="register"),
 ]
--- owlnookuser/views.py
+++ owlnookuser/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render, redirect
+from django.views import generic
 from django.contrib.auth import login, authenticate
 
 from .forms import RegistrationForm
 
 
-def registration_view(request):
-    context = {}
-    if request.method == "POST":
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            email = form.cleaned_data.get("email")
-            raw_password = form.cleaned_data.get("password1")
-            account = authenticate(email=email, password=raw_password)
-            login(request, account)
-            return redirect("home")
+class UserRegisterView(generic.CreateView):
+    form_class = RegistrationForm
+    template_name = "user/register.html"
+    context = {"form": form_class}
+
+    def post(self, request, *args, **kwargs):
+        context = dict(registration_form=self.form_class)
+        if request.method == "POST":
+            form = RegistrationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                email = form.cleaned_data.get("email")
+                raw_password = form.cleaned_data.get("password1")
+                account = authenticate(email=email, password=raw_password)
+                login(request, account)
+                return redirect("home")
+            else:
+                context["form"] = form
         else:
-            context["registration_form"] = form
-    else:
-        form = RegistrationForm()
-        context["registration_form"] = form
-    return render(request, "user/register.html", context)
+            form = RegistrationForm()
+            context["form"] = form
+        return render(request, "user/register.html", context)
