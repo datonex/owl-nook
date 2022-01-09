0970ea6b83f0c58455f3039e30ce6ef83775ab47
DatoneX <cgomwe@ymail.com>
2022-01-09 18:24:22 +0000

    Add UserAdmin class

--- owlnookuser/admin.py
+++ owlnookuser/admin.py
@@ -1,75 +1,28 @@
-from django.contrib import admin
 from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.core.exceptions import ValidationError
 
-from owlnookuser.models import OwlNookUser
-
-# https://docs.djangoproject.com/en/3.2/topics/auth/customizing/#custom-users-and-the-built-in-auth-forms
-class UserCreationForm(forms.ModelForm):
-    """A form for creating new users. Includes all the required
-    fields, plus a repeated password."""
-
-    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
-    password2 = forms.CharField(
-        label="Password confirmation", widget=forms.PasswordInput
-    )
-
-    class Meta:
-        model = OwlNookUser
-        fields = (
-            "username",
-            "email",
-        )
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
-
-class UserChangeForm(forms.ModelForm):
-    """A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    disabled password hash display field.
-    """
-
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = OwlNookUser
-        fields = ("email", "password", "username", "is_active", "is_admin")
+from .forms import RegistrationForm, EditUserForm
+from .models import OwlNookUser
 
 
 class UserAdmin(BaseUserAdmin):
-
-    form = UserChangeForm
-    add_form = UserCreationForm
+    model = OwlNookUser
+    form = EditUserForm
+    add_form = RegistrationForm
 
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ("username", "email", "is_admin")
-    list_filter = ("is_admin",)
+    list_display = ("username", "email", "is_admin", "is_active")
+    list_filter = ("is_admin", "is_active")
     fieldsets = (
         (
-            None,
+            "Login details",
             {
                 "fields": (
                     "email",
-                    "username",
                     "password",
                 )
             },
@@ -83,7 +36,20 @@ class UserAdmin(BaseUserAdmin):
                 )
             },
         ),
-        ("Permissions", {"fields": ("is_admin",)}),
+        (
+            "Profile",
+            {
+                "fields": (
+                    "username",
+                    "avatar_image",
+                    "biography",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {"fields": ("is_active", "is_staff", "is_admin", "is_superuser")},
+        ),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
@@ -92,7 +58,15 @@ class UserAdmin(BaseUserAdmin):
             None,
             {
                 "classes": ("wide",),
-                "fields": ("email", "username", "password1", "password2"),
+                "fields": (
+                    "email",
+                    "username",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_admin",
+                    "is_active",
+                ),
             },
         ),
     )
@@ -102,17 +76,9 @@ class UserAdmin(BaseUserAdmin):
         "first_name",
         "last_name",
     )
-    ordering = (
-        "email",
-        "username",
-        "first_name",
-        "last_name",
-    )
+    ordering = ("email",)
     filter_horizontal = ()
 
 
 # Now register the new UserAdmin...
 admin.site.register(OwlNookUser, UserAdmin)
-# ... and, since we're not using Django's built-in permissions,
-# unregister the Group model from admin.
-admin.site.unregister(Group)
